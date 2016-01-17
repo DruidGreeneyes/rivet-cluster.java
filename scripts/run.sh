31b071d98fcf7212aa13fed30f8af1547dbf9e9c
Steve Carton <steve@chesbay.net>
2016-01-17 13:18:02 -0500

    script tweak

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############
-# Riveting
+# Riveting. First arg is path to the source
 ##############
 echo "Rivet, Rivet"
 
@@ -9,5 +9,5 @@ cd ..
 mvn -DskipTests clean package
 
 #echo "$(cygpath -pw "$CLASSPATH")"
-java -Xmx10g -Dfile.encoding=UTF-8 -classpath "$(cygpath -pw "$CLASSPATH")" testing.Program 
+java -Xmx10g -Dfile.encoding=UTF-8 -classpath "$(cygpath -pw "$CLASSPATH")" testing.Program  $1
 
